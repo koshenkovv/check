dfab2746877e55c337ed55bb4f0a79b5ffc3efe5
koshenkovv <113175964+koshenkovv@users.noreply.github.com>
2023-11-30 12:51:04 +0300

    Add files via upload

---
+++ assets/scripts/project_script.js
@@ -0,0 +1,43 @@
+const el = document.getElementById('scroll-container');
+    let isDown = false;
+    let startX;
+    let scrollLeft;
+  
+    el.addEventListener('mousedown', (e) => {
+      isDown = true;
+      el.classList.add('active');
+      startX = e.pageX - el.offsetLeft;
+      scrollLeft = el.scrollLeft;
+    });
+  
+    el.addEventListener('mouseleave', () => {
+      isDown = false;
+    });
+  
+    el.addEventListener('mouseup', () => {
+      isDown = false;
+    });
+  
+    el.addEventListener('mousemove', (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - el.offsetLeft;
+      const walk = (x - startX); // remove the * 3 for direct following
+      el.scrollLeft = scrollLeft - walk;
+    });
+
+    function scrollToStart(elementId) {
+      // Get the element by ID
+      var element = document.getElementById('scroll-container');
+  
+      // Check if the element exists
+      if (element) {
+          // Smoothly scroll to the start (leftmost position)
+          element.scrollTo({
+              left: 0, // Scroll to the start (0 position horizontally)
+              behavior: 'smooth' // Enable smooth scrolling
+          });
+      } else {
+          console.error('Element not found with ID:', elementId);
+      }
+  }
